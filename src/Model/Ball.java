16c8f0f827664f054c3e2b6d90c8711f082d2b83
Tien Cu <TCu@student.neumont.edu>
2017-02-13 10:17:51 -0700

    more ball stuff

--- src/Model/Ball.java
+++ src/Model/Ball.java
@@ -1,10 +1,12 @@
 package Model;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.Random;
 
 import javax.swing.JPanel;
 import javax.swing.Timer;
@@ -14,25 +16,46 @@ import javax.swing.Timer;
 
 
 public class Ball extends JPanel implements ActionListener, MouseListener{
+	private static Random gen = new Random();
 	private int x = 1280, y = 657, xVelocity = 20, yVelocity = 20, width = 65, height = 65;
 	private Timer timer = new Timer((int) (30), this);
+	private boolean right = true;
+	public int player1 = 0;
+	public int player2 = 0;
 	
 	public Ball(){
 		
 	}
 	
 	@Override
-	public void paintComponent(Graphics g){
+	public void paint(Graphics g){
 		super.paint(g);
+		g.setColor(Color.black);
 		g.fillOval(x, y, width, height);
 		this.addMouseListener(this);
 	}
 	
 	public void actionPerformed(ActionEvent e){
-		x += xVelocity;
-		y += yVelocity;
-		if(x >= (this.getWidth() - width) || x <= 0){
-			xVelocity *= -1;
+		if(right == true){
+			x += xVelocity;
+			y += yVelocity;
+		}else if(right == false){
+			x -= xVelocity;
+			y += yVelocity;
+		}
+		if(x >= (this.getWidth() - width)){
+			player1++;
+			timer.stop();
+			x = 1280;
+			y = 657;
+			right = false;
+		}
+		if(x <= 0){
+			player2++;
+			timer.stop();
+			x = 1280;
+			y = 657;
+			right = true;
 		}
 		if(y >= (this.getHeight() - height) || y <= 0){
 			yVelocity *= -1;
